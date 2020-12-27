2d40db53bcc57b0f5c6acde0d36ff9d8061e2a1a
Şəmistan Hüseynov <shamistan1999@gmail.com>
2020-12-27 18:52:29 +0400

    --Initial structure --can be changed later

---
+++ src/main/java/app.java
@@ -0,0 +1,12 @@
+import figures.Figure;
+
+import java.util.Arrays;
+
+public class app {
+    public static void main(String[] args) {
+        System.out.println("Salam necesiniz");
+        Figure [] figures = Figure.values();
+        System.out.println(Arrays.toString(figures));
+
+    }
+}
---
+++ src/main/java/figures/Shape.java
@@ -0,0 +1,12 @@
+package figures;
+
+import java.awt.*;
+
+public class Shape {
+    Point p;
+    Color color;
+    int width;
+    int height;
+    boolean isFilled;
+
+}
